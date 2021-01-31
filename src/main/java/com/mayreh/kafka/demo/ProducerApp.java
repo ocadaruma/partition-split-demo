c4b369f17e299aa36684e6d74d95aefc98c23bbc
ocadaruma <ocadaruma@gmail.com>
2021-01-31 14:20:02 +0900

    fix

--- src/main/java/com/mayreh/kafka/demo/ProducerApp.java
+++ src/main/java/com/mayreh/kafka/demo/ProducerApp.java
@@ -45,7 +45,7 @@ public class ProducerApp extends Thread implements AutoCloseable {
 
             try {
                 RecordMetadata recordMeta = producer.send(record).get();
-                System.out.println(String.format("[ID=%d] sent. tp=%s-%d[%d]",
+                System.out.println(String.format("[ID=%d] sent. tp=%s-%d[offset=%d]",
                                                  id, recordMeta.topic(), recordMeta.partition(), recordMeta.offset()));
                 Thread.sleep(1000L);
             } catch (Exception e) {
